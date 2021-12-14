4fa3817da054fec913714ecbb8294b1d22b0d282
David Della Rossa <david.dellarossa@gmail.com>
2021-12-14 08:22:02 +0000

    Creating pawn enemy (wip)

---
+++ Fightship Arena/Assets/Scripts/Enemies/IEnemyControllerCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightShipArena.Assets.Scripts.Enemies
+{
+    public interface IEnemyControllerCore
+    {
+        public int Strenght { get; set; }
+        public int Health { get; set; }
+    }
+}
---
+++ Fightship Arena/Assets/Scripts/Enemies/PawnControllerCore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightShipArena.Assets.Scripts.Enemies
+{
+    public class PawnControllerCore : IEnemyControllerCore
+    {
+        public readonly IMyMonoBehaviour Parent;
+
+        public PawnControllerCore(IMyMonoBehaviour parent)
+        {
+            Parent = parent;
+        }
+
+        public int Strenght { get; set; }
+        public int Health { get; set; }
+    }
+}
