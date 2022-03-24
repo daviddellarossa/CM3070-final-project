b109f2470a373142e59060fc9720ed2156316f86
David Della Rossa <david.dellarossa@gmail.com>
2022-03-24 08:15:24 +0100

    Weapon classes commented

--- Fightship Arena/Assets/Scripts/Weapons/BulletSettings.cs
+++ Fightship Arena/Assets/Scripts/Weapons/BulletSettings.cs
@@ -7,12 +7,30 @@ using UnityEngine;
 
 namespace FightShipArena.Assets.Scripts.Weapons
 {
+    /// <summary>
+    /// Settings for a bullet
+    /// </summary>
     [CreateAssetMenu(fileName = "New Bullet InitSettings", menuName = "Weapons/Bullet InitSettings")]
     public class BulletSettings : ScriptableObject
     {
+        /// <summary>
+        /// Weapon type the bullet applies to
+        /// </summary>
         public WeaponType WeaponType;
+
+        /// <summary>
+        /// Speed of the bullet
+        /// </summary>
         public float Speed;
+
+        /// <summary>
+        /// Damage applied on collision
+        /// </summary>
         public int Damage;
+
+        /// <summary>
+        /// Scale factor for the object size
+        /// </summary>
         public float Scale;
     }
 }
