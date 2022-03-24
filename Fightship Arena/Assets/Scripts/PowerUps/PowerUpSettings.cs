c70424e3eba7429eea9dadce455bb45ade121c90
David Della Rossa <david.dellarossa@gmail.com>
2022-03-24 08:08:01 +0100

    Power-up classes commented

--- Fightship Arena/Assets/Scripts/PowerUps/PowerUpSettings.cs
+++ Fightship Arena/Assets/Scripts/PowerUps/PowerUpSettings.cs
@@ -7,11 +7,25 @@ using UnityEngine;
 
 namespace FightShipArena.Assets.Scripts.PowerUps
 {
+    /// <summary>
+    /// Settings for a power-up
+    /// </summary>
     [CreateAssetMenu(fileName = "New Power-Up InitSettings", menuName = "Power-Ups/Power-Up InitSettings")]
     public class PowerUpSettings : ScriptableObject
     {
+        /// <summary>
+        /// Power-up type
+        /// </summary>
         public PowerUpType PowerUpType;
+
+        /// <summary>
+        /// Value carried over
+        /// </summary>
         public float Value;
+
+        /// <summary>
+        /// Lifetime
+        /// </summary>
         public float Duration;
     }
 }
